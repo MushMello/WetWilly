48da502ca7e5d4092e5e28185917ea8da35d369d
David Browning <45465851+synth-the-humanoid@users.noreply.github.com>
2025-01-25 17:17:21 -0500

    update sceneloader + create test build

--- Assets/Title/SceneButtonHandler.cs
+++ Assets/Title/SceneButtonHandler.cs
@@ -1,4 +1,3 @@
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,21 +5,19 @@ using UnityEngine.UI;
 public class SceneButtonHandler : MonoBehaviour
 {
     [Header("Button Settings")]
-    [SerializeField] private SceneAsset targetScene;
+    [SerializeField] private int sceneBuildNumber;
 
+    private Scene scene;
     private Button attachedButton;
 
     private void Start()
     {
-        if (targetScene != null)
+        attachedButton = GetComponent<Button>();
+        if (attachedButton)
         {
-            attachedButton = GetComponent<Button>();
-            if(attachedButton)
-            {
-                attachedButton.onClick.AddListener(() => {
-                    SceneManager.LoadSceneAsync(targetScene.name);
+            attachedButton.onClick.AddListener(() => {
+                SceneManager.LoadSceneAsync(sceneBuildNumber);
                 });
-            }
         }
     }
 }
